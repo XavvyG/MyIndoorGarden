dac50abb6ad167dce0d634fbc9920505c235058f
swagGer83 <xavierkym@gmail.com>
2021-12-02 23:00:38 +0000

    added config, changes to app and added Plant model class

--- src/main/java/com/example/demo/MyIndoorGardenApplication.java
+++ src/main/java/com/example/demo/MyIndoorGardenApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyIndoorGardenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyIndoorGardenApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(MyIndoorGardenApplication.class, args);
+		
+		//Method name is the name of the Bean method
+		String s = ac.getBean("init").toString();
+		System.out.println(s);
 	}
 
 }
--- src/main/java/com/example/demo/data/model/Plant.java
+++ src/main/java/com/example/demo/data/model/Plant.java
@@ -1,5 +1,90 @@
 package com.example.demo.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Plant {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Integer id; // this is the primary key in a MySql DB
+	private String name;
+	private Integer quantity;
+	private String colour;
+	private String countryOfOrigin;
+	private Integer wateringFrequency;// in days
+	
+	public Plant() {
+	}
+	
+	public Plant(Integer id, String name, Integer quantity, String colour, String countryOfOrigin,
+			Integer wateringFrequency) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.colour = colour;
+		this.countryOfOrigin = countryOfOrigin;
+		this.wateringFrequency = wateringFrequency;
+	}
+
+	public Plant(String name, Integer quantity, String colour, String countryOfOrigin, Integer wateringFrequency) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+		this.colour = colour;
+		this.countryOfOrigin = countryOfOrigin;
+		this.wateringFrequency = wateringFrequency;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	public Integer getWateringFrequency() {
+		return wateringFrequency;
+	}
+
+	public void setWateringFrequency(Integer wateringFrequency) {
+		this.wateringFrequency = wateringFrequency;
+	}
 }
