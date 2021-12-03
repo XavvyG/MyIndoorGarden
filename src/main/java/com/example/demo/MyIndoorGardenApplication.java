622263eb8992311ecd97209e7adcf0f7c215507b
swagGer83 <xavierkym@gmail.com>
2021-12-03 13:04:04 +0000

    added README.md and JSONfields.txt

--- src/main/java/com/example/demo/MyIndoorGardenApplication.java
+++ src/main/java/com/example/demo/MyIndoorGardenApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyIndoorGardenApplication {
@@ -10,5 +9,4 @@ public class MyIndoorGardenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyIndoorGardenApplication.class, args);
 	}
-
 }
